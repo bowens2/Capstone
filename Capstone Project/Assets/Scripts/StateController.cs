b551a797fd18da4311521428ec6f3b856810fd00
Brandon Owens <42878673+bowens2@users.noreply.github.com>
2022-12-01 14:06:13 -0600

    Floor 1 Update Part 1
    partial updates to floor 1, resolving github issues

--- Capstone Project/Assets/Scripts/StateController.cs
+++ Capstone Project/Assets/Scripts/StateController.cs
@@ -4,5 +4,50 @@ using UnityEngine;
 
 public class StateController : MonoBehaviour
 {
-    public static string testString;
+    public static bool initDone = false;
+    
+    // Door Statuses
+    public static bool meetingRoomDoorOpen;
+    public static bool officeBlockDoorOpen;
+    public static bool DerekDoorOpen;
+    public static bool SandyDoorOpen;
+
+    // Player Position Maintaining (WIP)
+    public Rigidbody2D rb;
+    public static Vector2 Floor1PlayerPosition;
+    public static Vector2 Floor2PlayerPosition;
+
+    // Puzzle Hint Progress
+    public static bool receptionNoteFound;
+    public static bool showingEncodedText1;
+    public static bool meetingRoomNoteFound;
+    public static bool showingEncodedText2;
+
+    private void Start() {
+        if(initDone == false) {
+            initDone = true;
+            
+            meetingRoomDoorOpen = false;
+            officeBlockDoorOpen = false;
+            DerekDoorOpen = false;
+            SandyDoorOpen = false;
+
+            Floor1PlayerPosition.x = (float)7.0;
+            Floor1PlayerPosition.y = (float)-15.75;
+
+            receptionNoteFound = false;
+            showingEncodedText1 = false;
+            meetingRoomNoteFound = false;
+            showingEncodedText2 = false;
+
+            Debug.Log("initial values set");
+        }
+    }
+
+    //private void Update() {
+    //    Floor1PlayerPosition.x = rb.position.x;
+    //    Floor1PlayerPosition.y = rb.position.y;
+    //}
+    
+    
 }
---
+++ Capstone Project/Assets/Scripts/Triggers/MeetingRoomDoor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeetingRoomDoor : MonoBehaviour
+{
+
+    [SerializeField] private DoorAnimated door;
+    public Transform player;
+
+    public float detectionRange = 3.0f;
+    bool closeEnough;
+    bool doorOpen;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        doorOpen = door.animator.GetBool("Open");
+        closeEnough = false;
+
+        if(Vector3.Distance(player.position, transform.position) <= detectionRange && StateController.meetingRoomDoorOpen == true){
+            closeEnough = true;
+        }
+        if(closeEnough && Input.GetKeyDown(KeyCode.F) && !doorOpen) {
+            door.OpenDoor();
+            Debug.Log("opening door");
+        }
+        if(closeEnough && Input.GetKeyDown(KeyCode.F) && doorOpen) {
+            door.CloseDoor();
+            Debug.Log("closing door");
+        }
+    }
+}
